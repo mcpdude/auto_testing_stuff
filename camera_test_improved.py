9db32ac9173afa966085f220b9e5693f62382441
Brian Posey <mcpdude@gmail.com>
2018-08-13 16:41:45 -0700

    changed the test wrapper to call a function for the driver test.

--- camera_test_improved.py
+++ camera_test_improved.py
@@ -1,5 +1,6 @@
 import paramiko
 import datetime
+import subprocess
 from time import sleep
 from gpiozero import LED
  
@@ -34,75 +35,81 @@ def main():
  
     while True:
 
-        
-        #main_relay.off()
-        #sleep(1)
-        main_relay.on()
-        connected = False
-        while connected == False:
-            try:
-                ssh = paramiko.SSHClient()
-                ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                ssh.connect(host, username = user, password = password)
-                print("connected!")
-                connected = True
-                sleep(10)
-                print('running command')
-                stdin, stdout, stderr = ssh.exec_command(camera_check)
-                out_put = stdout.readlines()
-                print(out_put)
- 
-                if '/dev/ttyS2\n' in out_put:
-                    print('not insane')
- 
-                    if '/dev/video0\n' in out_put:
-                        print("Pass!")
-                        passes += 1
-
-                    else:
-                        print("Fail :(")
-                        fails += 1
-
-                else:
-                    print('invalid result')
-
-                log_file = open(filename, 'a')
-                trial_number = passes + reboots + fails + 1
-                log_out = str("Trial #: " + str(trial_number) + ", passes:", str(passes), ", fails:", str(fails), ", reboots:", str(reboots), "\n")
-                log_file.write(log_out)
-
-
- 
-            except:
-                
-                print("Retrying Connection... Attempt #:", connection_attempts)
-                sleep(1)
-                connected = False
-                connection_attempts += 1
-                if connection_attempts >=50:
-                    print("Couldn't connect, rebooting.")
-                    main_relay.on()
-                    main_relay.off()
-                    sleep(10)
-                    main_relay.on()
-                    connection_attempts = 0
-                    reboots += 1
+        driver_test()
         
         
-        connection_attempts = 0
-        ssh.close()
-        main_relay.on()
-        main_relay.off()
-        print("waiting ... (oven should be off)")
-        sleep(15)
-        print('Passes: ', passes)
-        print("Fails: ", fails)
-        print("Total: ", passes + fails)
-        if fails >= 1:
-            print("Fail rate: ", (fails / (fails + passes)) * 100,"%")
-            
-        if reboots >= 1:
-            print("Reboots: ", reboots)
 
 main()
+
+def driver_test():
+
+    #main_relay.off()
+    #sleep(1)
+    main_relay.on()
+    connected = False
+    while connected == False:
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(host, username = user, password = password)
+            print("connected!")
+            connected = True
+            sleep(10)
+            print('running command')
+            stdin, stdout, stderr = ssh.exec_command(camera_check)
+            out_put = stdout.readlines()
+            print(out_put)
+
+            if '/dev/ttyS2\n' in out_put:
+                print('not insane')
+
+                if '/dev/video0\n' in out_put:
+                    print("Pass!")
+                    passes += 1
+
+
+                else:
+                    print("Fail :(")
+                    fails += 1
+
+            else:
+                print('invalid result')
+
+            log_file = open(filename, 'a')
+            trial_number = passes + reboots + fails + 1
+            log_out = str("Trial #: " + str(trial_number) + ", passes:", str(passes), ", fails:", str(fails), ", reboots:", str(reboots), "\n")
+            log_file.write(log_out)
+
+
+
+        except:
+            
+            print("Retrying Connection... Attempt #:", connection_attempts)
+            sleep(1)
+            connected = False
+            connection_attempts += 1
+            if connection_attempts >=50:
+                print("Couldn't connect, rebooting.")
+                main_relay.on()
+                main_relay.off()
+                sleep(10)
+                main_relay.on()
+                connection_attempts = 0
+                reboots += 1
+    
+    
+    connection_attempts = 0
+    ssh.close()
+    main_relay.on()
+    main_relay.off()
+    print("waiting ... (oven should be off)")
+    sleep(15)
+    print('Passes: ', passes)
+    print("Fails: ", fails)
+    print("Total: ", passes + fails)
+    if fails >= 1:
+        print("Fail rate: ", (fails / (fails + passes)) * 100,"%")
+        
+    if reboots >= 1:
+        print("Reboots: ", reboots)
  
