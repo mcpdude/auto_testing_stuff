fac239c3391014b45aa2420e5d8c52a3878a9b8b
Brian Posey <mcpdude@gmail.com>
2018-08-22 11:38:40 -0700

    new relay control setup for ken

---
+++ relays.py
@@ -0,0 +1,54 @@
+from gpiozero import LED
+pins = {
+    "1": "18",
+    "2": "3", # Ken messed this up, yell at him if it borks
+    "3": "4",
+    "4": "17",
+    "5": "27",
+    "6": "22",
+    "7": "23",
+    "8": "24",
+    "9": "24",
+    "10": "5",
+    "11": "6",
+    "12": "13",
+    "13": "19",
+    "14": "26",
+    "15": "12",
+    "16": "16"
+
+}
+
+todo = ""
+which = ""
+
+
+while todo != "quit":
+    todo = input("Turn the relays on or off?")
+
+    if todo == "on":
+        which = input("Which one? Type all to turn them all on.\n")
+        if which == "all":
+            print("this don't work, sorry")
+
+        else:
+            print("Turning on relay:", pins.get(which, ""))
+            switch = LED(pins.get(which, ""))
+            switch.on()
+            print("Turned on the relay.")
+
+
+
+    elif todo == "off":
+        print("Turning the relays off.")
+        pin14.on()
+        pin15.on()
+        pin18.on()
+        pin23.on()
+        pin14.off()
+        pin15.off()
+        pin18.off()
+        pin23.off()
+
+    elif todo =="quit":
+        break
